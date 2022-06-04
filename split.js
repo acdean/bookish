664a706153e70b8d0d549f28b00e0fdeec0ed340
acdean <github@koogy.clara.co.uk>
2022-06-04 15:13:21 +0100

    Added hyphen

--- split.js
+++ split.js
@@ -10,7 +10,7 @@ function split(text) {
     // Hello World and Alice's (straight apostrophe)
     // groups of punctuation, including smart quotes
     // TODO &name; so i can leave mdashes etc in the samples
-    re = new RegExp("<[^>]*>|[A-Za-z0-9']{1,}|[ \.,!‘’“”—]{1,}", "g");
+    re = new RegExp("<[^>]*>|[A-Za-z0-9']{1,}|[ \.,!‘’“”—-]{1,}", "g");
     while (result = re.exec(text)) {
         out.push(result[0])
     }
